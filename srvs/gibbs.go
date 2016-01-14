3cb9a16d2df51bc25387ef6f7d2abe279e740609
Yi Wang <yi.wang.2005@gmail.com>
2016-01-13 23:57:14 -0800

    draft gibbs

--- srvs/gibbs.go
+++ srvs/gibbs.go
@@ -3,11 +3,29 @@ package srvs
 import (
 	"log"
 	"time"
+
+	"github.com/wangkuiyi/parallel"
 )
 
-func (wf *Master) gibbs(iter int) {
+func (m *Master) gibbs(iter int) {
 	log.Printf("Gibbs %d ...", iter)
 	start := time.Now()
+
+	ch := make(chan []string)
+	go func() {
+		for s := 0; s < len(m.corpusShards); s += m.cfg.VShards {
+			e := s + m.cfg.VShards
+			if e > len(m.corpusShards) {
+				e = len(m.corpusShards)
+			}
+			ch <- m.corpusShards[s:e]
+		}
+	}()
+	parallel.For(0, len(m.workers), m.cfg.VShards, func(w int) {
+		for seg := range ch {
+			sampleSegment(seg, m.workers[w:w+m.cfg.VShards], m.aggregators)
+		}
+	})
 
 	log.Printf("Gibbs %d done in %s", iter, time.Since(start))
 }
